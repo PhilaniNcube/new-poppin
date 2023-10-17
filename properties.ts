671525962a4f6a209500b0421d599d50c49ee30e
Philani Ncube <ncbphi001@gmail.com>
2023-10-17 19:06:28 +0200

    swap properties

--- properties.ts
+++ properties.ts
@@ -17,24 +17,7 @@ const properties = [
     baths: 2,
     price: 1500,
     images: ['/images/living.jpg', "/images/kitchen-small-3.jpg", "/images/kitchen-small.jpg", "/images/tv-1.jpg", "/images/welcome.jpg"]
-  },
-  {
-    slug: 'masingita_towers',
-    title: 'Masingita Towers',
-    description: 'A luxury apartment in the heart of Sandotn',
-    type: 'Apartment',
-        links: [ {
-      platform: "AirBnB",
-      href: "https://www.airbnb.co.za/rooms/46767440?locale=en&_set_bev_on_new_domain=1696280310_NWVmNTlhMjVjMmNh&source_impression_id=p3_1696280569_UwG9pgHxug4TqQPI"
-    }],
-    guests: 4,
-    bedrooms: 2,
-    beds: 2,
-    baths: 2.5,
-    price: 2580,
-    images: ["/images/masingita_towers/main_bedroom.jpeg", "/images/masingita_towers/living_room.jpeg", "/images/masingita_towers/sinks.jpeg", "/images/masingita_towers/balcony.jpeg", "/images/masingita_towers/bathroom_4.jpeg", "/images/masingita_towers/bed.jpeg", "/images/masingita_towers/balcony_2.jpeg", "/images/masingita_towers/bathroom.jpeg", "/images/masingita_towers/bedroom.jpeg", "/images/masingita_towers/dining.jpeg", "/images/masingita_towers/kitchen.jpeg", "/images/masingita_towers/living_room_2.jpeg", "/images/masingita_towers/smeg.jpeg"]
-  },
-  {
+  },  {
     slug: 'masingita_towers_2',
     title: 'Masingita Towers 2',
     description: 'Entire serviced apartment in Sandton, South Africa',
@@ -47,9 +30,26 @@ const properties = [
     bedrooms: 2,
     beds: 2,
     baths: 2.5,
-    price: 2780,
+    price: 2580,
   images: ["/images/masingita_towers_two/image_1.JPG", "/images/masingita_towers_two/image_2.JPG", "/images/masingita_towers_two/image_3.JPG", "/images/masingita_towers_two/image_4.JPG", "/images/masingita_towers_two/image_5.JPG", "/images/masingita_towers_two/image_6.JPG", "/images/masingita_towers_two/image_7.JPG", "/images/masingita_towers_two/image_8.JPG", "/images/masingita_towers_two/image_9.JPG", "/images/masingita_towers_two/image_10.JPG", "/images/masingita_towers_two/image_11.JPG", "/images/masingita_towers_two/image_12.JPG", "/images/masingita_towers_two/image_13.JPG", "/images/masingita_towers_two/image_14.JPG", "/images/masingita_towers_two/image_16.JPG"]
   },
+  {
+    slug: 'masingita_towers_3',
+    title: 'Masingita Towers 3',
+    description: 'A luxury apartment in the heart of Sandotn',
+    type: 'Apartment',
+        links: [ {
+      platform: "AirBnB",
+      href: "https://www.airbnb.co.za/rooms/46767440?locale=en&_set_bev_on_new_domain=1696280310_NWVmNTlhMjVjMmNh&source_impression_id=p3_1696280569_UwG9pgHxug4TqQPI"
+    }],
+    guests: 4,
+    bedrooms: 2,
+    beds: 2,
+    baths: 2.5,
+    price: 2780,
+    images: ["/images/masingita_towers/main_bedroom.jpeg", "/images/masingita_towers/living_room.jpeg", "/images/masingita_towers/sinks.jpeg", "/images/masingita_towers/balcony.jpeg", "/images/masingita_towers/bathroom_4.jpeg", "/images/masingita_towers/bed.jpeg", "/images/masingita_towers/balcony_2.jpeg", "/images/masingita_towers/bathroom.jpeg", "/images/masingita_towers/bedroom.jpeg", "/images/masingita_towers/dining.jpeg", "/images/masingita_towers/kitchen.jpeg", "/images/masingita_towers/living_room_2.jpeg", "/images/masingita_towers/smeg.jpeg"]
+  },
+
 ]
 
 export type Property = typeof properties[0]

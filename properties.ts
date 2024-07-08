07454ec0149269e679d58161cb31123e535a80d9
Philani Ncube <ncbphi001@gmail.com>
2024-07-08 20:28:58 +0200

    Update email field in contact forms

--- properties.ts
+++ properties.ts
@@ -1,84 +1,208 @@
 const properties = [
-  {
-    slug: 'bryanston',
-    title: 'Bryanston',
-    description: 'A beautiful apartment in the heart of Bryanston',
-    text: ['This modern apartment is conveniently located close to popular shopping malls like Bryanston and Morningside Shopping Centre as well as Nicolway. It&apos;s a mere 8km from Sandton City and the Sandton Gautrain Station.', 'Thanks to uncapped WiFi, kickback and enjoy your favourite shows on Netflix, Showmax, YouTube and other streaming sites from the comfort of the lounge or the main bedroom.'],
-    type: 'Apartment',
-    links: [{
-      platform: 'Booking.com',
-      href: "https://www.booking.com/hotel/za/pop-inn-modern-apartment-in-heart-of-bryanston.en-gb.html?aid=1258472&label=Share-lfWNCr%401630515174"
-    }, {
-      platform: "AirBnB",
-      href: "https://www.airbnb.co.za/rooms/44663589?source_impression_id=p3_1654077115_rSo%2BvOVoa16m5AE3&locale=en&_set_bev_on_new_domain=1654077110_Yjc1ZDNlZmM4NzBk"
-    }],
-    guests: 4,
-    bedrooms: 2,
-    beds: 2,
-    baths: 2,
-    price: 1500,
-    images: ['/images/bedroom-blue-2.jpg', "/images/bedroom-blue-3.jpg", "/images/kitchen-large.jpg", "/images/dining.jpg", "/images/welcome.jpg", "/images/building.jpg", "/images/bathroom-2.jpg", "/images/balcony.jpg", "/images/bathroom.jpg", "/images/bedroom-yellow-2.jpg", "/images/bedroom-yellow.jpg", "/images/kitchen-coffee.jpg", "/images/living-2.jpg", "/images/lounge.jpg"]
-  },
-    {
-    slug: 'morningside_1',
-    title: 'Morningside 1',
-    description: 'contemporary 2 bedroom and 2 bathroom apartment is located in the heart of Sandton.',
-    text: ['This contemporary 2 bedroom and 2 bathroom apartment is located in the heart of Sandton. It is a 5 minute drive from Sandton City Mall as well as the Gautrain Station.', "The apartment includes uncapped Wifi, smart TVs and smeg appliances. Both bedrooms include an en-suite bathroom and there is a guest bathroom as well. This fully furnished apartment is ideal for those who enjoy luxury, comfort and convenience. Whether your travelling solo, as a couple, a family or on a business trip; this one is definitely the one to choose."],
-    type: 'Apartment',
-    links: [
-    //   {
-    //   platform: "AirBnB",
-    //   href: "https://www.airbnb.co.za/rooms/731588256049922111?locale=en&_set_bev_on_new_domain=1696280310_NWVmNTlhMjVjMmNh&source_impression_id=p3_1696280765_OoToFRfrLlU4tuZV"
-    // }
-  ],
-    guests: 4,
-    bedrooms: 2,
-    beds: 2,
-    baths: 2.5,
-    price: 2580,
-  images: ["/images/morningside_1/3.jpeg", "/images/morningside_1/1.jpeg", "/images/morningside_1/5.jpeg", "/images/morningside_1/4.jpeg", "/images/morningside_1/10.jpeg", "/images/morningside_1/6.jpeg", "/images/morningside_1/7.jpeg", "/images/morningside_1/8.jpeg", "/images/morningside_1/9.jpeg", "/images/morningside_1/2.jpeg", "/images/morningside_1/11.jpeg", "/images/morningside_1/12.jpeg", "/images/morningside_1/13.jpeg", "/images/morningside_1/14.jpeg"]
-  },
-  {
-    slug: 'morningside_2',
-    title: 'Morningside 2',
-    description: 'Entire serviced apartment in Sandton, South Africa',
-    text: ['This luxurious and spacious 2 bedroom, 2.5 bathroom apartment is located in the heart of Sandton. It is a 5 minute drive from Sandton City Mall as well as the Gautrain Station.', "This fully furnished apartment is ideal for the modern business individual, family or couple. The apartment includes uncapped Wifi, smart TVs and smeg appliances. Both bedrooms include an en-suite bathroom and there is a guest bathroom as well.", "This apartment is perfect for those that enjoy luxurious, comfort and convenience."],
-    type: 'Apartment',
-    links: [
-    //   {
-    //   platform: "AirBnB",
-    //   href: "https://www.airbnb.co.za/rooms/731588256049922111?locale=en&_set_bev_on_new_domain=1696280310_NWVmNTlhMjVjMmNh&source_impression_id=p3_1696280765_OoToFRfrLlU4tuZV"
-    // }
-  ],
-    guests: 4,
-    bedrooms: 2,
-    beds: 2,
-    baths: 2.5,
-    price: 2580,
-  images: ["/images/masingita_towers_two/image_1.JPG", "/images/masingita_towers_two/image_2.JPG", "/images/masingita_towers_two/image_3.JPG", "/images/masingita_towers_two/image_4.JPG", "/images/masingita_towers_two/image_5.JPG", "/images/masingita_towers_two/image_6.JPG", "/images/masingita_towers_two/image_7.JPG", "/images/masingita_towers_two/image_8.JPG", "/images/masingita_towers_two/image_9.JPG", "/images/masingita_towers_two/image_10.JPG", "/images/masingita_towers_two/image_11.JPG", "/images/masingita_towers_two/image_12.JPG", "/images/masingita_towers_two/image_13.JPG", "/images/masingita_towers_two/image_14.JPG"]
-  },
+	{
+		slug: "bryanston",
+		title: "Bryanston",
+		description: "A beautiful apartment in the heart of Bryanston",
+		text: [
+			"This modern apartment is conveniently located close to popular shopping malls like Bryanston and Morningside Shopping Centre as well as Nicolway. It&apos;s a mere 8km from Sandton City and the Sandton Gautrain Station.",
+			"Thanks to uncapped WiFi, kickback and enjoy your favourite shows on Netflix, Showmax, YouTube and other streaming sites from the comfort of the lounge or the main bedroom.",
+		],
+		type: "Apartment",
+		links: [
+			{
+				platform: "Booking.com",
+				href: "https://www.booking.com/hotel/za/pop-inn-modern-apartment-in-heart-of-bryanston.en-gb.html?aid=1258472&label=Share-lfWNCr%401630515174",
+			},
+			{
+				platform: "AirBnB",
+				href: "https://www.airbnb.co.za/rooms/44663589?source_impression_id=p3_1654077115_rSo%2BvOVoa16m5AE3&locale=en&_set_bev_on_new_domain=1654077110_Yjc1ZDNlZmM4NzBk",
+			},
+		],
+		guests: 4,
+		bedrooms: 2,
+		beds: 2,
+		baths: 2,
+		price: 1500,
+		images: [
+			"/images/bedroom-blue-2.jpg",
+			"/images/bedroom-blue-3.jpg",
+			"/images/kitchen-large.jpg",
+			"/images/dining.jpg",
+			"/images/welcome.jpg",
+			"/images/building.jpg",
+			"/images/bathroom-2.jpg",
+			"/images/balcony.jpg",
+			"/images/bathroom.jpg",
+			"/images/bedroom-yellow-2.jpg",
+			"/images/bedroom-yellow.jpg",
+			"/images/kitchen-coffee.jpg",
+			"/images/living-2.jpg",
+			"/images/lounge.jpg",
+		],
+	},
+	{
+		slug: "morningside_1",
+		title: "Morningside 1",
+		description:
+			"contemporary 2 bedroom and 2 bathroom apartment is located in the heart of Sandton.",
+		text: [
+			"This contemporary 2 bedroom and 2 bathroom apartment is located in the heart of Sandton. It is a 5 minute drive from Sandton City Mall as well as the Gautrain Station.",
+			"The apartment includes uncapped Wifi, smart TVs and smeg appliances. Both bedrooms include an en-suite bathroom and there is a guest bathroom as well. This fully furnished apartment is ideal for those who enjoy luxury, comfort and convenience. Whether your travelling solo, as a couple, a family or on a business trip; this one is definitely the one to choose.",
+		],
+		type: "Apartment",
+		links: [
+			//   {
+			//   platform: "AirBnB",
+			//   href: "https://www.airbnb.co.za/rooms/731588256049922111?locale=en&_set_bev_on_new_domain=1696280310_NWVmNTlhMjVjMmNh&source_impression_id=p3_1696280765_OoToFRfrLlU4tuZV"
+			// }
+		],
+		guests: 4,
+		bedrooms: 2,
+		beds: 2,
+		baths: 2.5,
+		price: 2580,
+		images: [
+			"/images/morningside_1/3.jpeg",
+			"/images/morningside_1/1.jpeg",
+			"/images/morningside_1/5.jpeg",
+			"/images/morningside_1/4.jpeg",
+			"/images/morningside_1/10.jpeg",
+			"/images/morningside_1/6.jpeg",
+			"/images/morningside_1/7.jpeg",
+			"/images/morningside_1/8.jpeg",
+			"/images/morningside_1/9.jpeg",
+			"/images/morningside_1/2.jpeg",
+			"/images/morningside_1/11.jpeg",
+			"/images/morningside_1/12.jpeg",
+			"/images/morningside_1/13.jpeg",
+			"/images/morningside_1/14.jpeg",
+		],
+	},
+	{
+		slug: "morningside_2",
+		title: "Morningside 2",
+		description: "Entire serviced apartment in Sandton, South Africa",
+		text: [
+			"This luxurious and spacious 2 bedroom, 2.5 bathroom apartment is located in the heart of Sandton. It is a 5 minute drive from Sandton City Mall as well as the Gautrain Station.",
+			"This fully furnished apartment is ideal for the modern business individual, family or couple. The apartment includes uncapped Wifi, smart TVs and smeg appliances. Both bedrooms include an en-suite bathroom and there is a guest bathroom as well.",
+			"This apartment is perfect for those that enjoy luxurious, comfort and convenience.",
+		],
+		type: "Apartment",
+		links: [
+			//   {
+			//   platform: "AirBnB",
+			//   href: "https://www.airbnb.co.za/rooms/731588256049922111?locale=en&_set_bev_on_new_domain=1696280310_NWVmNTlhMjVjMmNh&source_impression_id=p3_1696280765_OoToFRfrLlU4tuZV"
+			// }
+		],
+		guests: 4,
+		bedrooms: 2,
+		beds: 2,
+		baths: 2.5,
+		price: 2580,
+		images: [
+			"/images/masingita_towers_two/image_1.JPG",
+			"/images/masingita_towers_two/image_2.JPG",
+			"/images/masingita_towers_two/image_3.JPG",
+			"/images/masingita_towers_two/image_4.JPG",
+			"/images/masingita_towers_two/image_5.JPG",
+			"/images/masingita_towers_two/image_6.JPG",
+			"/images/masingita_towers_two/image_7.JPG",
+			"/images/masingita_towers_two/image_8.JPG",
+			"/images/masingita_towers_two/image_9.JPG",
+			"/images/masingita_towers_two/image_10.JPG",
+			"/images/masingita_towers_two/image_11.JPG",
+			"/images/masingita_towers_two/image_12.JPG",
+			"/images/masingita_towers_two/image_13.JPG",
+			"/images/masingita_towers_two/image_14.JPG",
+		],
+	},
 
-  {
-    slug: 'morningside_3',
-    title: 'Morningside 3',
-    description: 'A luxury apartment in the heart of Sandotn',
-   text: ['This luxurious and spacious apartment boasts 2 bedroom with King Beds as well as 2.5 bathrooms.  It is located in the heart of Sandton with Sandton City Mall and the Gautrain station being a mere 5 minute drive.', "This fully furnished apartment is ideal for the modern business individual, family or couple. The apartment includes uncapped Wifi, smart TVs and smeg appliances. Both bedrooms include an en-suite bathroom and there is a guest bathroom as well. ", "This apartment is perfect for those that enjoy luxurious, comfort and convenience."],
-    type: 'Apartment',
-        links: [
-    //       {
-    //   platform: "AirBnB",
-    //   href: "https://www.airbnb.co.za/rooms/46767440?locale=en&_set_bev_on_new_domain=1696280310_NWVmNTlhMjVjMmNh&source_impression_id=p3_1696280569_UwG9pgHxug4TqQPI"
-    // }
-  ],
-    guests: 4,
-    bedrooms: 2,
-    beds: 2,
-    baths: 2.5,
-    price: 2780,
-    images: ["/images/masingita_towers/main_bedroom.jpeg", "/images/masingita_towers/main_bedroom_3.jpeg", "/images/masingita_towers/living_room.jpeg", "/images/masingita_towers/sinks.jpeg", "/images/masingita_towers/balcony.jpeg", "/images/masingita_towers/bathroom_4.jpeg", "/images/masingita_towers/bed.jpeg", "/images/masingita_towers/balcony_2.jpeg", "/images/masingita_towers/bathroom.jpeg", "/images/masingita_towers/bedroom.jpeg", "/images/masingita_towers/dining.jpeg", "/images/masingita_towers/kitchen.jpeg", "/images/masingita_towers/living_room_2.jpeg", "/images/masingita_towers/smeg.jpeg", "/images/masingita_towers/kitchen_counter.jpeg", "/images/masingita_towers/pillow.jpeg", "/images/masingita_towers/lounge_3.jpeg"]
-  },
+	{
+		slug: "morningside_3",
+		title: "Morningside 3",
+		description: "A luxury apartment in the heart of Sandotn",
+		text: [
+			"This luxurious and spacious apartment boasts 2 bedroom with King Beds as well as 2.5 bathrooms.  It is located in the heart of Sandton with Sandton City Mall and the Gautrain station being a mere 5 minute drive.",
+			"This fully furnished apartment is ideal for the modern business individual, family or couple. The apartment includes uncapped Wifi, smart TVs and smeg appliances. Both bedrooms include an en-suite bathroom and there is a guest bathroom as well. ",
+			"This apartment is perfect for those that enjoy luxurious, comfort and convenience.",
+		],
+		type: "Apartment",
+		links: [
+			//       {
+			//   platform: "AirBnB",
+			//   href: "https://www.airbnb.co.za/rooms/46767440?locale=en&_set_bev_on_new_domain=1696280310_NWVmNTlhMjVjMmNh&source_impression_id=p3_1696280569_UwG9pgHxug4TqQPI"
+			// }
+		],
+		guests: 4,
+		bedrooms: 2,
+		beds: 2,
+		baths: 2.5,
+		price: 2780,
+		images: [
+			"/images/masingita_towers/main_bedroom.jpeg",
+			"/images/masingita_towers/main_bedroom_3.jpeg",
+			"/images/masingita_towers/living_room.jpeg",
+			"/images/masingita_towers/sinks.jpeg",
+			"/images/masingita_towers/balcony.jpeg",
+			"/images/masingita_towers/bathroom_4.jpeg",
+			"/images/masingita_towers/bed.jpeg",
+			"/images/masingita_towers/balcony_2.jpeg",
+			"/images/masingita_towers/bathroom.jpeg",
+			"/images/masingita_towers/bedroom.jpeg",
+			"/images/masingita_towers/dining.jpeg",
+			"/images/masingita_towers/kitchen.jpeg",
+			"/images/masingita_towers/living_room_2.jpeg",
+			"/images/masingita_towers/smeg.jpeg",
+			"/images/masingita_towers/kitchen_counter.jpeg",
+			"/images/masingita_towers/pillow.jpeg",
+			"/images/masingita_towers/lounge_3.jpeg",
+		],
+	},
 
-]
+	{
+		slug: "edenburg_villa",
+		title: "Edenburg Sandton Villa",
+		description:
+			"Luxury Holiday Accommodation in the heart of Johannesburg North",
+		text: [
+			"This 6 Bedroom 6 bathroom boutique hotel is available for full house holiday rentals, training and conferencing, corporate events and private gatherings.",
+			"A place of rest facing the Braamfontein Spruit with calming sounds of water flowing and birds singing in this untapped green oasis.",
+			"Sparkling pool and garden large enough to host private gatherings and corporate events.",
+		],
+		type: "Apartment",
+		links: [
+			//       {
+			//   platform: "AirBnB",
+			//   href: "https://www.airbnb.co.za/rooms/46767440?locale=en&_set_bev_on_new_domain=1696280310_NWVmNTlhMjVjMmNh&source_impression_id=p3_1696280569_UwG9pgHxug4TqQPI"
+			// }
+		],
+		guests: 12,
+		bedrooms: 6,
+		beds: 6,
+		baths: 6.5,
+		price: 12000,
+		images: [
+			"/images/edenburg/bedroom-7.webp",
+			"/images/edenburg/bedroom-1.webp",
+			"/images/edenburg/dining-1.jpeg",
+			"/images/edenburg/kitchen-1.webp",
+			"/images/edenburg/living-room-1.webp",
+			"/images/edenburg/bathroom-1.jpeg",
+			"/images/edenburg/living-room-2.webp",
+			"/images/edenburg/bedroom-5.webp",
+			"/images/edenburg/bathroom-2.webp",
+			"/images/edenburg/dining-2.webp",
+			"/images/edenburg/bedroom-3.webp",
+			"/images/edenburg/bedroom-4.webp",
+			"/images/edenburg/bedroom-2.jpeg",
+			"/images/edenburg/kitchen-2.jpeg",
+			"/images/edenburg/kitchen-3.jpeg",
+			"/images/edenburg/living-room-3.webp",
+			"/images/edenburg/bedroom-6.webp",
+			"/images/edenburg/bedroom-8.webp",
+		],
+	},
+];
 
 export type Property = typeof properties[0]
 
